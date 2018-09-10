880b1baf8d811ab056bfef008e9ff8c7a5801f78
gitlcq <lcq1130@163.com>
2018-09-10 16:04:03 +0800

    vue

---
+++ src/components/D_dbank/dbank.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>我是网盘</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "dbank"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/E_address/addressBook.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>我是通讯录</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "address"
+    }
+</script>
+
+<style scoped>
+
+</style>
